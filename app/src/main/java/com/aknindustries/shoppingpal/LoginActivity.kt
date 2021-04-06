c6f995da9b45aa470795030d16107f19c13a6075
Amlan <amlannandy5@gmail.com>
2021-04-06 09:22:28 +0530

    Built login screen ui

--- app/src/main/java/com/aknindustries/shoppingpal/LoginActivity.kt
+++ app/src/main/java/com/aknindustries/shoppingpal/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.aknindustries.shoppingpal
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
--- app/src/main/java/com/aknindustries/shoppingpal/SplashActivity.kt
+++ app/src/main/java/com/aknindustries/shoppingpal/SplashActivity.kt
@@ -1,14 +1,12 @@
 package com.aknindustries.shoppingpal
 
 import android.content.Intent
-import android.graphics.Typeface
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
-import android.widget.TextView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +25,7 @@ class SplashActivity : AppCompatActivity() {
 
         @Suppress("DEPRECATION")
         Handler().postDelayed({
-            intent = Intent(this@SplashActivity, MainActivity::class.java)
+            intent = Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000)
