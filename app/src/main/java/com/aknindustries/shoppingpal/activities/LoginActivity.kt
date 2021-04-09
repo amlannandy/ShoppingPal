ce54a4f6c6b7a788f0894aa25520d862ca88d43c
Amlan <amlannandy5@gmail.com>
2021-04-09 12:16:54 +0530

    Implemented login validation

--- app/src/main/java/com/aknindustries/shoppingpal/activities/LoginActivity.kt
+++ app/src/main/java/com/aknindustries/shoppingpal/activities/LoginActivity.kt
@@ -2,14 +2,15 @@ package com.aknindustries.shoppingpal.activities
 
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.TextView
 import com.aknindustries.shoppingpal.R
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -25,10 +26,37 @@ class LoginActivity : AppCompatActivity() {
         }
 
         // Set on click listener of Register button
-        val registerText :TextView = findViewById<TextView>(R.id.tv_register)
-        registerText.setOnClickListener {
+        tv_register.setOnClickListener {
             intent = Intent(this@LoginActivity, RegisterActivity::class.java)
             startActivity(intent)
+        }
+
+        // Set on click listen on Login Button
+        btn_login.setOnClickListener {
+            if (validateLogin()) {
+                showSnackBar("TEst success", false)
+            }
+        }
+    }
+
+    private fun validateLogin() : Boolean {
+        val email = findViewById<TextView>(R.id.et_email_id).text.toString().trim()
+        val password = findViewById<TextView>(R.id.et_password).text.toString().trim()
+        return when {
+            TextUtils.isEmpty(email) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+            // Check for valid email
+            !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_valid_email), true)
+                false
+            }
+            else -> true
         }
     }
 }
