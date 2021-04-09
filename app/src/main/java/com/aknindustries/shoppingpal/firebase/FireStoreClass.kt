c16242690b1837c671e6be39bd364aa135355e39
Amlan <amlannandy5@gmail.com>
2021-04-09 21:32:56 +0530

    Integrated Firestore

---
+++ app/src/main/java/com/aknindustries/shoppingpal/firebase/FireStoreClass.kt
@@ -0,0 +1,9 @@
+package com.aknindustries.shoppingpal.firebase
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FireStoreClass {
+
+    private val mFireStoreClass = FirebaseFirestore.getInstance()
+
+}
