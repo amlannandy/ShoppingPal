1afbee0840058165ae5e7e6cb4e3acf9e77f5441
Amlan <amlannandy5@gmail.com>
2021-04-13 22:16:06 +0530

    Implemented permission handling

--- app/src/main/java/com/aknindustries/shoppingpal/activities/ProfileActivity.kt
+++ app/src/main/java/com/aknindustries/shoppingpal/activities/ProfileActivity.kt
@@ -1,13 +1,16 @@
 package com.aknindustries.shoppingpal.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.aknindustries.shoppingpal.R
 import com.aknindustries.shoppingpal.models.User
 import com.aknindustries.shoppingpal.utils.Constants
 import kotlinx.android.synthetic.main.activity_profile.*
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -29,5 +32,34 @@ class ProfileActivity : AppCompatActivity() {
         et_email.isEnabled = false
         et_email.setText(user.email)
 
+        iv_user_photo.setOnClickListener(this@ProfileActivity)
+
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_user_photo -> {
+                    if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showSnackBar("The storage permission is granted.", false)
+            } else {
+                showSnackBar(resources.getString(R.string.read_storage_permission_denied), false)
+            }
+        }
     }
 }
--- app/src/main/java/com/aknindustries/shoppingpal/utils/Constants.kt
+++ app/src/main/java/com/aknindustries/shoppingpal/utils/Constants.kt
@@ -6,5 +6,6 @@ object Constants {
     const val APP_PREFERENCES = "ShoppingPal"
     const val LOGGED_IN_USERNAME = "logged_in_username"
     const val EXTRA_USER_DETAILS = "extra_user_details"
+    const val READ_STORAGE_PERMISSION_CODE = 2
 
 }
