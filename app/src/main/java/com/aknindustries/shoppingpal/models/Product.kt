17589dad4f45e7c9762be3331815ff23dc8abb9b
Amlan <amlannandy5@gmail.com>
2021-04-20 09:44:19 +0530

    Created Product model

---
+++ app/src/main/java/com/aknindustries/shoppingpal/models/Product.kt
@@ -0,0 +1,16 @@
+package com.aknindustries.shoppingpal.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class Product (
+    val id : String = "",
+    val userId : String = "",
+    var userName : String = "",
+    val title : String = "",
+    val description : String = "",
+    val price : Double = 0.0,
+    val quantity : Int = 0,
+    val imageUrl : String = "",
+) : Parcelable
